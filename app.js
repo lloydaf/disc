9a4924bd96851445d20b34d443c9189e0575cdf0
Lloyd Francis <lloydaf@gmail.com>
2016-08-19 05:02:07 +0000

    Resolved Conflicts. Error Commit

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/disc');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var admin = require('./routes/admin');
@@ -26,12 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/admin.js
+++ routes/admin.js
@@ -4,51 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res, next){
 	res.render('enter',{ pagenum : '1' });
 });
-
-/* POST to Add User Service */
-router.post('/enterpage', function(req, res){
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var row1Text = req.body.row1text;
-    var row1Most = req.body.row1most;
-    var row1Least = req.body.row1least;
-    var row2Text = req.body.row2text;
-    var row2Most = req.body.row2most;
-    var row2Least = req.body.row2least;
-    var row3Text = req.body.row3text;
-    var row3Most = req.body.row3most;
-    var row3Least = req.body.row3least;
-    var row4Text = req.body.row4text;
-    var row4Most = req.body.row4most;
-    var row4Least = req.body.row4least;
-    var pageNo = req.body.pageno;
-
-
-    // Set our collection
-    var collection = db.collection('pagedata');
-    // Submit to the DB
-    collection.insert({
-        "pageno" : pageNo,
-        "data" : {
-        	"row1text" : row1Text,
-        	"row1most" : row1Most,
-        	"row1least" : row1Least,
-        	"row2text" : row2Text,
-        	"row2most" : row2Most,
-        	"row2least" : row2Least,
-        	"row3text" : row3Text,
-        	"row3most" : row3Most,
-        	"row3least" : row3Least,
-        	"row4text" : row4Text,
-        	"row4most" : row4Most,
-        	"row4least" : row4Least
-        }
-    }, function (err, doc) {
-        
-    });
-    res.render('enter', { pagenum : ++pageNo });
+router.post('/enterpage', function(req, res, next){
+	res.render('enter');
 });
+
 module.exports = router;
