1487e12f07d9ebad8f27400586e5e9c2144d5b46
Lloyd Francis <lloydaf@gmail.com>
2016-08-19 05:08:40 +0000

    Resolved Conflicts, New Stable Commit
    Resolved Conflicts, Enabled entering page data into database depending
    on user input

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/disc');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var admin = require('./routes/admin');
-
 var app = express();
 
 // view engine setup
@@ -22,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var pageno=0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DISC' });
-});
-var pageno=0;
-router.get('/page', function(req, res){
+	pageno=0;
 	++pageno;
 	var db = req.db;
-	var query = {};
-	query["pageno"] = pageno.toString();
-	db.collection('pagedata').findOne(query, function(err, values){
-	var locals = {
-		"pagenum" : pageno,
-		"row1text" : values.data.row1text,
-		"row1most" : values.data.row1most,
-		"row1least" : values.data.row1least,
-		"row2text" : values.data.row2text,
-		"row2most" : values.data.row2most,
-		"row2least" : values.data.row2least,
-		"row3text" : values.data.row3text,
-		"row3most" : values.data.row3most,
-		"row3least" : values.data.row3least,
-		"row4text" : values.data.row4text,
-		"row4most" : values.data.row4most,
-		"row4least" : values.data.row4least
-	};	
-	res.render('page', locals);
-	});
+	db.collection('map').drop();
+	res.render('index', { title: 'DISC' });
+});
+router.get('/page', function(req, res){
+	if(pageno==29){
+		res.render('complete');
+	}
+	else{	
+		var db = req.db;
+		var query = {};
+		query["pageno"] = pageno.toString();
+		db.collection('pagedata').findOne(query, function(err, values){
+		var locals = {
+			"pagenum" : pageno,
+			"row1text" : values.data.row1text,
+			"row1most" : values.data.row1most,
+			"row1least" : values.data.row1least,
+			"row2text" : values.data.row2text,
+			"row2most" : values.data.row2most,
+			"row2least" : values.data.row2least,
+			"row3text" : values.data.row3text,
+			"row3most" : values.data.row3most,
+			"row3least" : values.data.row3least,
+			"row4text" : values.data.row4text,
+			"row4most" : values.data.row4most,
+			"row4least" : values.data.row4least
+		};	
+		res.render('page', locals);
+		});
+	}
+});
 
+router.post('/update', function(req, res){
+	var most = req.body.most;
+	var least = req.body.least;
+	var pagenum = req.body.pagenum;
+	var db = req.db;
+	var query = {};
+	query["pageno"] = pagenum.toString();
+	collection = db.collection('map');
+	collection.findOne(query, function(err, values){
+		if(values){
+			res.send('Error!');
+		}
+		else{
+			var map = db.collection('map');
+			map.insert({
+				"pageno" : pagenum,
+				"most" : most,
+				"least" : least 
+			});
+			++pageno;
+			res.redirect('/page');
+		}
+	});
 });
 
 module.exports = router;
